115fc324277fb4ad54d3c748ae43e40eb20a2417
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2024-01-12 18:39:53 +0900

    * add header and footer.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Footer from './components/footer';
+import Header from './components/header';
 import CreateItem from './pages/item/create';
 import DeleteItem from './pages/item/delete';
 import ReadAll from './pages/item/readAll';
@@ -11,15 +13,19 @@ import Register from './pages/user/register';
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/user/register" element={<Register />} />
-        <Route path="/user/login" element={<Login />} />
-        <Route path="/" element={<ReadAll />} />
-        <Route path="/item/:id" element={<ReadSingle />} />
-        <Route path="/item/create" element={<CreateItem />} />
-        <Route path="/item/update/:id" element={<UpdateItem />} />
-        <Route path="/item/delete/:id" element={<DeleteItem />} />
-      </Routes>
+      <div className="container">
+        <Header />
+        <Routes>
+          <Route path="/user/register" element={<Register />} />
+          <Route path="/user/login" element={<Login />} />
+          <Route path="/" element={<ReadAll />} />
+          <Route path="/item/:id" element={<ReadSingle />} />
+          <Route path="/item/create" element={<CreateItem />} />
+          <Route path="/item/update/:id" element={<UpdateItem />} />
+          <Route path="/item/delete/:id" element={<DeleteItem />} />
+        </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 };
---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+// footer.js
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>©{new Date().getFullYear()} MERN Market</p>
+    </footer>
+  );
+};
+
+export default Footer;
